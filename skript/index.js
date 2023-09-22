d8c6e59f3d94066bc80db9b2a3fae23c62cefa09
nimnyll <michko.daniil@gmail.com>
2023-09-22 16:46:59 +0300

    first commit

--- skript/index.js
+++ skript/index.js
@@ -18,6 +18,91 @@
 //  }
 
 
+// class Animal {
+//     legs = 4;
+//     name = "";
+//     maxSpeed = 50;
+    
+//     constructor(name, maxSpeed, legs){
+//         this.legs = legs
+//         this.maxSpeed = maxSpeed
+//         this.name = name
+//     }
+
+//     eat = function (){
+//         alert("я їм")
+//     }
+
+//     run(){
+//         alert(`моя швідкість - ${this.maxSpeed}`)
+//     }
+
+//     mylegs(){
+//         alert(`у мене е ${this.legs} ноги`)
+//     }
+// }
+// class Parrot extends Animal {
+//     fly(){
+//         alert("я літаю!")
+//     }
+// }
+
+
+
+// let animal2 = new Animal("Dag", 15, 4)
+// anmal2.run();
+// animal2.mylegs();
+
+// let animal1 = new Parrot("Rembo", 30, 2)
+// animal1.fly();
+// animal1.run();
+
+
+// class Car {
+//     obem = 0;
+//     marka = "";
+//     speed = 0
+
+//     constructor(obem, marka, speed){
+//         this.obem = obem;
+//         this.marka = marka;
+//         this.speed = speed;
+//     }
+
+//     Obem(){
+//         alert(`обем двигуна = ${this.obem}`)
+//     }
+
+//     Marka(){
+//         alert(`марка машини ${this.marka}`)
+//     }
+
+//     Speed(){
+//         alert(`швидкість = ${this.speed}`)
+//     }
+// }
+// class Biyk extends Car {
+//     bmv(){
+//         alert("також це байк")
+//     }
+// }
+
+// let car = new Car(20, "WV", 100)
+// let car1 = new Car(25, "Fort", 80)
+
+// car.Obem();
+// car.Marka();
+// car.Speed();
+
+// car1.Obem();
+// car1.Marka();
+// car1.Speed();
+// let car2 = new Biyk(40, "BMW", 140)
+
+// car2.Obem();
+// car2.Marka();
+// car2.Speed();
+// car2.bmv();
 
 
 
